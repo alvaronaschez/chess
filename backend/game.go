0a3e4cc2a62059586cbbdb97449abe7e88c9c8fd
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2024-06-10 16:21:19 +0200

    refactor: move message struct to game file

--- backend/game.go
+++ backend/game.go
@@ -13,6 +13,14 @@ type ChessGame struct {
 	blackReadChannel chan Message
 }
 
+type Message struct {
+	Type      string `json:"type" validate:"required,oneof=start move"`
+	Color     string `json:"color" validate:"oneof=white black,required_if=Type start"`
+	From      string `json:"from" validate:"required_if=Type move"`
+	To        string `json:"to" validate:"required_if=Type move"`
+	Promotion string `json:"promotion" validate:"oneof=q r b k,required_if=Type move"`
+}
+
 func NewChessGame(ws *websocket.Conn) *ChessGame {
 	game := ChessGame{
 		whiteWebsocket:   ws,
--- backend/message.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type Message struct {
-	Type      string `json:"type" validate:"required,oneof=start move"`
-	Color     string `json:"color" validate:"oneof=white black,required_if=Type start"`
-	From      string `json:"from" validate:"required_if=Type move"`
-	To        string `json:"to" validate:"required_if=Type move"`
-	Promotion string `json:"promotion" validate:"oneof=q r b k,required_if=Type move"`
-}
