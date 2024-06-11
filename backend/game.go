c2757fcbc40188d8ec1499d389de910fd09d136f
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2024-06-11 16:13:28 +0200

    refactor: improve ChessGame code

--- backend/game.go
+++ backend/game.go
@@ -7,10 +7,8 @@ import (
 )
 
 type ChessGame struct {
-	whiteWebsocket   *websocket.Conn
-	blackWebsocket   *websocket.Conn
-	whiteReadChannel chan Message
-	blackReadChannel chan Message
+	whiteWebsocket *websocket.Conn
+	blackWebsocket *websocket.Conn
 }
 
 type Message struct {
@@ -22,47 +20,47 @@ type Message struct {
 }
 
 func NewChessGame(ws *websocket.Conn) *ChessGame {
-	game := ChessGame{
-		whiteWebsocket:   ws,
-		whiteReadChannel: make(chan Message),
-		blackReadChannel: make(chan Message),
-	}
+	game := ChessGame{whiteWebsocket: ws}
 	return &game
 }
 
 func (game *ChessGame) Join(ws *websocket.Conn) {
 	game.blackWebsocket = ws
-	go forwardFromWebsocketToChannel(game.whiteWebsocket, game.whiteReadChannel)
-	go forwardFromWebsocketToChannel(game.blackWebsocket, game.blackReadChannel)
-	go func() {
-		turnWhite := true
-		game.whiteWebsocket.WriteJSON(Message{Type: "start", Color: "white"})
-		game.blackWebsocket.WriteJSON(Message{Type: "start", Color: "black"})
-		for {
-			select {
-			case message := <-game.whiteReadChannel:
-				if message.Type == "error" {
-					log.Println("error")
-					return
-				}
-				if turnWhite {
-					game.blackWebsocket.WriteJSON(message)
-					turnWhite = false
-				}
-			case message := <-game.blackReadChannel:
-				if message.Type == "error" {
-					return
-				}
-				if !turnWhite {
-					game.whiteWebsocket.WriteJSON(message)
-					turnWhite = true
-				}
-			}
-		}
-	}()
+	whiteChannel := make(chan Message)
+	blackChannel := make(chan Message)
+	go playChess(game.whiteWebsocket, game.blackWebsocket, whiteChannel, blackChannel)
+	go forwardFromWebsocketToChannel(game.whiteWebsocket, whiteChannel)
+	go forwardFromWebsocketToChannel(game.blackWebsocket, blackChannel)
 }
 
-func forwardFromWebsocketToChannel(ws *websocket.Conn, ch chan Message) {
+func playChess(whiteWebsocket, blackWebsocket *websocket.Conn, whiteChannel, blackChannel <-chan Message) {
+	turnWhite := true
+	whiteWebsocket.WriteJSON(Message{Type: "start", Color: "white"})
+	blackWebsocket.WriteJSON(Message{Type: "start", Color: "black"})
+	for {
+		select {
+		case message := <-whiteChannel:
+			if message.Type == "error" {
+				log.Println("error")
+				return
+			}
+			if turnWhite {
+				blackWebsocket.WriteJSON(message)
+				turnWhite = false
+			}
+		case message := <-blackChannel:
+			if message.Type == "error" {
+				return
+			}
+			if !turnWhite {
+				whiteWebsocket.WriteJSON(message)
+				turnWhite = true
+			}
+		}
+	}
+}
+
+func forwardFromWebsocketToChannel(ws *websocket.Conn, ch chan<- Message) {
 	defer ws.Close()
 	for {
 		message := Message{}
